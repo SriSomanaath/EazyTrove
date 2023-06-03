e41f4146f3caa21e7daf49a96632b2fac65763df
SriSomanaath <govindalapudisrinath@gmail.com>
2023-06-03 22:12:06 +0530

    second commit

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router} from '@angular/router';
+import {SignUp} from '../data-type';
+
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
-  styleUrls: ['./seller-auth.component.css']
+  styleUrls: ['./seller-auth.component.css'] 
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit  {
+  constructor(private seller:SellerService,private router:Router){}
 
+  ngOnInit():void{}
+
+
+
+  signUp(data:SignUp):void{
+    this.seller.userSignUp(data).subscribe((result)=>{
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+    })
+  }
 }
